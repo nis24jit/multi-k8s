033e18069d160617f648171072b3c4495dd9c59d
Nishanth <nishanth.suresh.thanayil.punathil@nn.nl>
2021-05-15 14:32:40 +0200

    added deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
-docker build -t cygnetops/multi-client-k8s:latest -t cygnetops/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t cygnetops/multi-server-k8s-pgfix:latest -t cygnetops/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t cygnetops/multi-worker-k8s:latest -t cygnetops/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t dockerhound/multi-client:latest -t dockerhound/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t dockerhound/multi-server:latest -t dockerhound/multi-server:$SHA  -f ./server/Dockerfile ./server
+docker build -t dockerhound/multi-worker:latest -t dockerhound/multi-worker:$SHA  -f ./worker/Dockerfile ./worker
 
-docker push cygnetops/multi-client-k8s:latest
-docker push cygnetops/multi-server-k8s-pgfix:latest
-docker push cygnetops/multi-worker-k8s:latest
+docker push dockerhound/multi-client:latest
+docker push dockerhound/multi-server:latest
+docker push dockerhound/multi-worker:latest
 
-docker push cygnetops/multi-client-k8s:$SHA
-docker push cygnetops/multi-server-k8s-pgfix:$SHA
-docker push cygnetops/multi-worker-k8s:$SHA
+docker push dockerhound/multi-client:$SHA
+docker push dockerhound/multi-server:$SHA
+docker push dockerhound/multi-worker:$SHA
+
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=cygnetops/multi-server-k8s-pgfix:$SHA
-kubectl set image deployments/client-deployment client=cygnetops/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=cygnetops/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=dockerhound/multi-server:$SHA
+kubectl set image deployments/client-deployment client=dockerhound/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=dockerhound/multi-worker:$SHA
